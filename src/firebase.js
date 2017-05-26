a3cafc7f151fdc0778cecbdc277ec9d1cbc2ffa8
Ryan Sullivan <ryan@ryansully.com>
2017-05-26 06:21:31 -0700

    Add Firebase configuration and instances

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+import * as firebaseui from 'firebaseui';
+
+export const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+
+export const defaultApp = firebase.initializeApp(config);
+export const ui = new firebaseui.auth.AuthUI(firebase.auth());
+export default firebase;
